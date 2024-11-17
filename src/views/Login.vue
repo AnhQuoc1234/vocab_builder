d5a7d1f7b5bbad901262fa60167b4c36c084b326
AnhQuoc1234 <134694138+AnhQuoc1234@users.noreply.github.com>
2024-11-17 17:08:43 +0700

    Add files via upload

---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h1>Login</h1>
+      <form @submit.prevent="handleLogin">
+        <input v-model="username" placeholder="Username" required />
+        <input v-model="password" type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+    methods: {
+      handleLogin() {
+        // Perform actual authentication here (e.g., API call)
+        const user = { username: this.username };
+        this.$store.dispatch('login', user);
+        this.$router.push('/dashboard');
+      },
+    },
+  };
+  </script>
