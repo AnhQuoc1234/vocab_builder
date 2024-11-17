f551d36171f4f851c1ed02c4ada4cad2a7caf75b
AnhQuoc1234 <134694138+AnhQuoc1234@users.noreply.github.com>
2024-11-17 17:14:14 +0700

    Add files via upload

---
+++ front-end/src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to your dashboard, {{ user?.username }}!</p>
+      <button @click="logout">Logout</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    computed: {
+      user() {
+        return this.$store.getters.user;
+      },
+    },
+    methods: {
+      logout() {
+        this.$store.dispatch('logout');
+        this.$router.push('/login');
+      },
+    },
+  };
+  </script>
